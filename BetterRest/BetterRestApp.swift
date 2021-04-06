5a7df467f29c45c6bb6d2c6d5a4214d1c19958b1
Pranav Kasetti <pranavkasetti@gmail.com>
2021-04-06 19:10:45 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pranav Kasetti on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
